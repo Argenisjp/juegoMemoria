9c9bac94866bd9cd15819c36ac47f794b72088cd
Argenisjp <barsainvicto@gmail.com>
2022-09-18 22:21:58 -0500

    subimos actualizacion con imagenes y sonidos

--- main.js
+++ main.js
@@ -12,6 +12,15 @@ let timerInicial = 60;
 let tiempoRegresivoId = null;
 
 
+let victoriaAudio = new Audio('./sonidos/victoria.mp3');
+let fatalityAudio = new Audio('./sonidos/fatality.mp3');
+let overAudio = new Audio('./sonidos/mario-game-over.mp3');
+let yajuAudio = new Audio('./sonidos/yaju.mp3');
+let fallaAudio = new Audio('./sonidos/falla.mp3');
+let click2Audio = new Audio('./sonidos/click2.mp3');
+
+
+
 // Apuntando a Movimientos de HTML
 let mostrarMovimientos = document.getElementById('movimientos');
 let mostraraciertos = document.getElementById('aciertos')
@@ -30,6 +39,7 @@ function contarTiempo(){
         if (timer == 0) {
             clearInterval(tiempoRegresivoId);
             bloquearTargetas();
+            overAudio.play();
         }
     },1000)
 }
@@ -37,7 +47,7 @@ function contarTiempo(){
 function bloquearTargetas(){
     for (let i = 0  ; i <= 15;   i++)  {
         let targetaBloqueada = document.getElementById(i);
-        targetaBloqueada.innerHTML = numeros[i];
+        targetaBloqueada.innerHTML = `<img src="./img/${numeros[i]}.png">`;
         targetaBloqueada.disabled = true;
 
 
@@ -59,41 +69,49 @@ function destapar(id){
 
 
     if (targetasDestapadas == 1) {
+        
         // Mostramos primer numero
         targeta1 = document.getElementById(id);
         primerResultado = numeros[id]
-        targeta1.innerHTML = primerResultado;
-
+        targeta1.innerHTML = `<img src="./img/${primerResultado}.png">`;
+        click2Audio.play();
+        
         // Desabilitamos primer boton
         targeta1.disabled = true;
 
     }else if (targetasDestapadas == 2) {
+        click2Audio.play();
         targeta2 = document.getElementById(id);
         segundoResultado = numeros[id];
-        targeta2.innerHTML = segundoResultado;
+        targeta2.innerHTML = `<img src="./img/${segundoResultado}.png">`;
+       
 
         targeta2.disabled = true;
-
         // Incrementamos movimientos
         movimientos ++;
         mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
 
 
         if (primerResultado == segundoResultado) {
+             yajuAudio.play();
             // encerrar contador tagetas destapadas
             targetasDestapadas = 0;
 
             aciertos ++;
             mostraraciertos.innerHTML = `Aciertos: ${aciertos}`;
+           
 
             if (aciertos == 8) {
+                victoriaAudio.play(); //sonido cuando se gana el juego
                 clearInterval(tiempoRegresivoId);
                 mostraraciertos.innerHTML = `Aciertos: ${aciertos} 🎈🎉🎊😁` ;
                 mostrarTiempo.innerHTML = `Coño ganaste 😎 solo te demoraste ${timerInicial - timer} segundos`
                 mostrarMovimientos.innerHTML = `Movimientos: ${movimientos} 😎`;
+                
             }
 
         }else{
+            fallaAudio.play();
             // Mostrar momentaneamente y volver a tapar 
             setTimeout(()=>{
                 targeta1.innerHTML = ' ';
